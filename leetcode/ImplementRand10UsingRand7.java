cd827e5d98ed2ffe8247aa591126f8aeeebb741c
Sim MyoungJin <37402215+myeongmy@users.noreply.github.com>
2020-08-31 23:55:20 +0900

    leetcode 470 solved(math)

---
+++ leetcode/ImplementRand10UsingRand7.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * The rand7() API is already defined in the parent class SolBase. public int
+ * rand7();
+ * 
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+	public int rand10() {
+		int random = 7 * (rand7() - 1) + rand7();
+
+		while (random >= 41)
+			random = 7 * (rand7() - 1) + rand7();
+		return (random - 1) % 10 + 1;
+	}
+}
